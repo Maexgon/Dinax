b1eb975df3258bc795fcee35b6d790449b613c28
Mariano Gonzalez <mgonzalez@nativadigital.com>
2025-12-15 16:50:21 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2649429281-36673",
+  "appId": "1:951211951452:web:300876b7675feb30c26d99",
+  "apiKey": "AIzaSyDiiqlEDPntAWjKLUPf2ge40GlCmARFDkc",
+  "authDomain": "studio-2649429281-36673.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "951211951452"
+};
